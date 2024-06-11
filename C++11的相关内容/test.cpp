7b54e3ad5c1cd03b371ae8a58b63803ebca64a10
wlkkt <yangyf0618@qq.com>
2024-06-11 23:37:55 +0800

    完善

--- C++11的相关内容/test.cpp
+++ C++11的相关内容/test.cpp
@@ -395,28 +395,29 @@
 //    return 0;
 //}
 
-//#include <iostream>
-//using namespace std;
-//void Fun(int& x) { cout << "左值引用" << endl; }
-//void Fun(const int& x) { cout << "const 左值引用" << endl; }
-//void Fun(int&& x) { cout << "右值引用" << endl; }
-//void Fun(const int&& x) { cout << "const 右值引用" << endl; }
-//
-//template<typename T>
-//void PerfectForward(T&& t)
-//{
-//    Fun(t);
-//}
-//
-//int main()
-//{
-//    PerfectForward(10);//右值，预计调用void Fun(int&& x) { cout << "右值引用" << endl; }
-//    int a;
-//    PerfectForward(a); //左值
-//    PerfectForward(move(a)); // 右值，预计调用void Fun(int&& x) { cout << "右值引用" << endl; }
-//    const int b = 8;
-//    PerfectForward(b);//左值
-//    PerfectForward(move(b)); // const 右值，预计调用void Fun(const int&& x) { cout << "const 右值引用" << endl; }
-//    return 0;
-//}
+#include <iostream>
+using namespace std;
+void Fun(int& x) { cout << "左值引用" << endl; }
+void Fun(const int& x) { cout << "const 左值引用" << endl; }
+void Fun(int&& x) { cout << "右值引用" << endl; }
+void Fun(const int&& x) { cout << "const 右值引用" << endl; }
+
+template<typename T>
+void PerfectForward(T&& t)
+{
+   /* Fun(t);*/
+    Fun(forward<T>(t));
+}
+
+int main()
+{
+    PerfectForward(10);//右值，预计调用void Fun(int&& x) { cout << "右值引用" << endl; }
+    int a;
+    PerfectForward(a); //左值
+    PerfectForward(move(a)); // 右值，预计调用void Fun(int&& x) { cout << "右值引用" << endl; }
+    const int b = 8;
+    PerfectForward(b);//左值
+    PerfectForward(move(b)); // const 右值，预计调用void Fun(const int&& x) { cout << "const 右值引用" << endl; }
+    return 0;
+}
 
